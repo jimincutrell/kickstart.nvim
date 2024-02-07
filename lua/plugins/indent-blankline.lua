e8cb9d5ec57c57ccc519736c0e960f56b6075eee
Jimin Cutrell <jimin.cutrell@gmail.com>
2024-02-06 23:05:59 -0800

    Fix breaking changes for indent-blankline v3

--- lua/plugins/indent-blankline.lua
+++ lua/plugins/indent-blankline.lua
@@ -3,8 +3,13 @@ return {
   'lukas-reineke/indent-blankline.nvim',
   -- Enable `lukas-reineke/indent-blankline.nvim`
   -- See `:help indent_blankline.txt`
+  main = "ibl",
   opts = {
-    char = '┊',
-    show_trailing_blankline_indent = false
+    indent = {
+      char = '┊',
+    },
+    whitespace = {
+      remove_blankline_trail = false
+    },
   },
 }
