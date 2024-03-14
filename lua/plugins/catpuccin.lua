9d035e3400bd0a38237c4a5c337d7dd98692db7d
jimincutrell <jimin.cutrell@gmail.com>
2024-03-13 20:24:09 -0700

    Switch theme to catppuccin

---
+++ lua/plugins/catpuccin.lua
@@ -0,0 +1,13 @@
+return {
+  "catppuccin/nvim",
+  name = "catppuccin",
+  priority = 1000,
+  config = function()
+    require("catppuccin").setup({
+      color_overrides = {
+        mocha = { base = "#000000" }
+      }
+    })
+    vim.cmd.colorscheme("catppuccin-mocha")
+  end
+}
