6698dc57fe40cc201aa9003a986b5edcec26a4bd
Jimin Cutrell <jimin.cutrell@gmail.com>
2024-06-17 12:23:31 -0700

    Add linting basic + fix lua file locations

---
+++ lua/options.lua
@@ -0,0 +1,56 @@
+-- [[ Setting options ]]
+-- See `:help vim.o`
+
+-- Set highlight on search
+vim.o.hlsearch = false
+vim.o.incsearch = true
+
+-- Make line numbers default
+vim.wo.number = true
+vim.opt.relativenumber = true
+
+-- Indent options
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.smartindent = true
+
+vim.opt.wrap = false
+vim.opt.scrolloff = 8
+
+-- Enable mouse mode
+vim.o.mouse = 'a'
+
+-- Sync clipboard between OS and Neovim.
+--  Remove this option if you want your OS clipboard to remain independent.
+--  See `:help 'clipboard'`
+vim.o.clipboard = 'unnamedplus'
+
+-- Enable break indent
+vim.o.breakindent = true
+
+-- Show indents
+vim.opt.list = true
+vim.opt.listchars:append 'space:·'
+
+-- Save undo history
+vim.o.undofile = true
+
+-- Case insensitive searching UNLESS /C or capital in search
+vim.o.ignorecase = true
+vim.o.smartcase = true
+
+-- Keep signcolumn on by default
+vim.wo.signcolumn = 'yes'
+
+-- Decrease update time
+vim.o.updatetime = 50
+--vim.o.timeout = true
+--vim.o.timeoutlen = 100
+
+-- Set completeopt to have a better completion experience
+vim.o.completeopt = 'menuone,noselect'
+
+-- NOTE: You should make sure your terminal supports this
+vim.o.termguicolors = true
+
