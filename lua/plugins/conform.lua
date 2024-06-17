19200fc38af610194da9f89f9d94aca7e5b2acde
Jimin Cutrell <jimin.cutrell@gmail.com>
2024-06-17 15:46:13 -0700

    Add conform.nvim

---
+++ lua/plugins/conform.lua
@@ -0,0 +1,22 @@
+-- For autoformatting
+return {
+	'stevearc/conform.nvim',
+	opts = {
+		notify_on_error = false,
+		format_on_save = {
+			timeout_ms = 500,
+			lsp_fallback = true
+		}
+	},
+	formatters_by_ft = {
+		lua = { 'stylua' },
+		-- Conform can also run multiple formatters sequentially
+		-- python = { "isort", "black" },
+		--
+		-- You can use a sub-list to tell conform to run *until* a formatter
+		-- is found.
+		-- javascript = { { "prettierd", "prettier" } },
+		javascript = { { 'eslint_d' } },
+		typescript = { { 'eslint_d' } }
+	},
+}
